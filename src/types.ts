b4353d8e2cc2478d686ce2e32c38c3c800430c6e
Katrina H. Turner <46706381+KatrinaTurner@users.noreply.github.com>
2021-10-19 20:35:22 -1000

    add layer2 variables

--- src/types.ts
+++ src/types.ts
@@ -1,15 +1,25 @@
 export interface MapOptions {
   color: string;
-  mapjson: string;
-  nodeHighlight: string;
-  srcField: string;
-  dstField: string;
-  valField: string;
-  endpointId: string;
+  mapjsonL1: string;
+  nodeHighlightL1: string;
+  srcFieldL1: string;
+  dstFieldL1: string;
+  valFieldL1: string;
+  endpointIdL1: string;
+  nodeWidthL1: number;
+  edgeWidthL1: number;
+  pathOffsetL1: number;
+  layer2: boolean;
+  mapjsonL2: string;
+  nodeHighlightL2: string;
+  srcFieldL2: string;
+  dstFieldL2: string;
+  valFieldL2: string;
+  endpointIdL2: string;
+  nodeWidthL2: number;
+  edgeWidthL2: number;
+  pathOffsetL2: number;
   startLat: number;
   startLng: number;
   startZoom: number;
-  nodeWidth: number;
-  edgeWidth: number;
-  pathOffset: number;
 }
