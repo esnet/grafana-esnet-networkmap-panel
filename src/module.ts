99e1d46fcfb6ba0bb3619cfcc2d180b2f94b34ed
Katrina H. Turner <46706381+KatrinaTurner@users.noreply.github.com>
2021-11-03 08:35:47 -1000

    add layer 3

--- src/module.ts
+++ src/module.ts
@@ -6,8 +6,9 @@ const FieldsCategory = ['Choose Fields'];
 const LayersCategory = ['Layer options'];
 
 export const plugin = new PanelPlugin<MapOptions>(MapPanel);
-const layer2Bool = (layer2: boolean) => (config: MapOptions) => config.layer2 === layer2;
 const layer1Bool = (layer1: boolean) => (config: MapOptions) => config.layer1 === layer1;
+const layer2Bool = (layer2: boolean) => (config: MapOptions) => config.layer2 === layer2;
+const layer3Bool = (layer3: boolean) => (config: MapOptions) => config.layer3 === layer3;
 
 plugin.setPanelOptions((builder) => {
   builder.addNumberInput({
@@ -182,6 +183,155 @@ plugin.setPanelOptions((builder) => {
       min: 1,
       max: 15,
       step: 0.5,
+    },
+  });
+
+  // Layer 3
+  builder.addBooleanSwitch({
+    path: 'layer3',
+    name: 'Layer 3 on',
+    category: LayersCategory,
+    defaultValue: false,
+  });
+  builder.addTextInput({
+    path: 'mapjsonL3',
+    name: 'Layer 3 Map data (json)',
+    category: LayersCategory,
+    description: 'JSON with edges and nodes of network map',
+    showIf: layer3Bool(true),
+    defaultValue: '',
+  });
+  builder.addColorPicker({
+    path: 'color3',
+    name: 'Layer 3 Default color',
+    category: LayersCategory,
+    showIf: layer3Bool(true),
+    description: 'The default color for nodes and links on Layer 3',
+    defaultValue: 'grey',
+  });
+  builder.addTextInput({
+    path: 'endpointIdL3',
+    name: 'Layer 3 Endpoint Identifier',
+    category: LayersCategory,
+    description: 'The endpoint identifier in the meta data to match to the query',
+    showIf: layer3Bool(true),
+    defaultValue: 'router',
+  });
+  builder.addColorPicker({
+    path: 'nodeHighlightL3',
+    name: 'Layer 3 Node highlight color',
+    category: LayersCategory,
+    description: 'The color to highlight nodes that match the query',
+    showIf: layer3Bool(true),
+    defaultValue: 'red',
+  });
+  builder.addSliderInput({
+    path: 'nodeWidthL3',
+    name: 'Layer 3 Node Size',
+    category: LayersCategory,
+    showIf: layer3Bool(true),
+    defaultValue: 5,
+    settings: {
+      min: 1,
+      max: 15,
+      step: 0.5,
+    },
+  });
+  builder.addSliderInput({
+    path: 'edgeWidthL3',
+    name: 'Layer 3 Edge Width',
+    category: LayersCategory,
+    showIf: layer3Bool(true),
+    defaultValue: 3,
+    settings: {
+      min: 1,
+      max: 15,
+      step: 0.5,
+    },
+  });
+  builder.addSliderInput({
+    path: 'pathOffsetL3',
+    name: 'Layer 3 Edge Offset',
+    description: 'The offset between AZ path and ZA path',
+    category: LayersCategory,
+    showIf: layer2Bool(true),
+    defaultValue: 3,
+    settings: {
+      min: 1,
+      max: 15,
+      step: 0.5,
+    },
+  });
+  builder.addSelect({
+    path: 'srcFieldL3',
+    name: 'Layer 3 Source Field',
+    description: 'Select the field to match source nodes',
+    category: FieldsCategory,
+    showIf: layer3Bool(true),
+    settings: {
+      allowCustomValue: false,
+      options: [],
+      getOptions: async (context: FieldOverrideContext) => {
+        const options: any[] = [];
+        if (context && context.data) {
+          for (const frame of context.data) {
+            for (const field of frame.fields) {
+              const name = getFieldDisplayName(field, frame, context.data);
+              const value = name;
+              options.push({ value, label: name });
+            }
+          }
+        }
+        return Promise.resolve(options);
+      },
+    },
+  });
+  builder.addSelect({
+    path: 'dstFieldL3',
+    name: 'Layer 3 Destination Field',
+    description: 'Select the field to match destination nodes',
+    category: FieldsCategory,
+    showIf: layer3Bool(true),
+    settings: {
+      allowCustomValue: false,
+      options: [],
+      getOptions: async (context: FieldOverrideContext) => {
+        const options: any[] = [];
+        if (context && context.data) {
+          for (const frame of context.data) {
+            for (const field of frame.fields) {
+              const name = getFieldDisplayName(field, frame, context.data);
+              const value = name;
+              options.push({ value, label: name });
+            }
+          }
+        }
+        return Promise.resolve(options);
+      },
+    },
+  });
+  builder.addSelect({
+    path: 'valFieldL3',
+    name: 'Layer 3 Value Field',
+    description: 'Select the field to use for data values',
+    category: FieldsCategory,
+    showIf: layer3Bool(true),
+    settings: {
+      allowCustomValue: false,
+      options: [],
+      getOptions: async (context: FieldOverrideContext) => {
+        const options: any[] = [];
+        if (context && context.data) {
+          for (const frame of context.data) {
+            for (const field of frame.fields) {
+              const name = getFieldDisplayName(field, frame, context.data);
+              const value = name;
+              options.push({ value, label: name });
+            }
+          }
+        }
+        return Promise.resolve(options);
+      },
     },
   });
 
