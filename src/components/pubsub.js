111c38d4ea56eca288c9d9d3e6dfc4d188346f8d
James Kafader <jkafader@es.net>
2022-03-16 15:44:31 -0700

    properly modularize this functionality; more 'professional' version; use a singleton pattern.

--- src/components/pubsub.js
+++ src/components/pubsub.js
@@ -1,21 +1,45 @@
-window.__pubsub = {
-    "topics": {
-    },
-    "subscribe": function(topic, callback){
-        // use toLocaleString as a (good) approximation of a unique hash so we don't
-        // resubscribe to the same topic on code load over and over.
-        var hash = callback.toLocaleString();
-        if(!window.__pubsub.topics[topic]){
-            window.__pubsub.topics[topic] = {hash: callback}
-        } else {
-            window.__pubsub.topics[topic][hash] = callback;
-        }
-    },
-    "publish": function(topic, eventData){
-        console.log("publishing event", eventData, "on topic", topic);
-        var subscribers = Object.values(window.__pubsub.topics[topic]);
-        for(var i=0; subscribers && i<subscribers.length; i++){
-            subscribers[i](eventData);
-        }
+class PrivateMessageBus {
+    constructor(){
+        this.topics = {}
+        this.lastEvents = {}
     }
 }
+
+PrivateMessageBus.prototype.subscribe = function(topic, callback){
+    // use toLocaleString as a (good) approximation of a unique hash so we don't
+    // resubscribe to the same topic on code load over and over.
+    var hash = callback.toLocaleString();
+    if(!this.topics[topic]){
+        this.topics[topic] = {hash: callback}
+    } else {
+        this.topics[topic][hash] = callback;
+    }
+}
+
+PrivateMessageBus.prototype.publish = function(topic, eventData){
+    console.log("publishing event", eventData, "on topic", topic);
+    this.lastEvents[topic] = eventData;
+    var subscriberData = this.topics[topic];
+    if (!subscriberData) return;
+    var subscribers = Object.values(subscriberData);
+    for(var i=0; subscribers && i<subscribers.length; i++){
+        subscribers[i](eventData);
+    }
+}
+
+// returns the last eventData value for a particular topic
+PrivateMessageBus.prototype.last = function(topic){
+    return this.lastEvents[topic];
+}
+
+var messageBus = null;
+
+function getInstance() {
+    if(!messageBus){
+        messageBus = new PrivateMessageBus();
+    }
+    return messageBus;
+}
+
+module.exports.PubSub = getInstance();
+exports.PubSub = getInstance();
