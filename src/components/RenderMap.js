10d63f33af63c0b150547b546b9484388c5d7dc0
James Kafader <jkafader@es.net>
2022-04-18 09:20:23 -0700

    add tilesets, political boundaries, labels. Subscribe to signals for Node/Edge edit

--- src/components/RenderMap.js
+++ src/components/RenderMap.js
@@ -7,8 +7,100 @@ import React from 'react';
 import { locationService } from '@grafana/runtime';
 import { PubSub } from 'components/pubsub.js';
 
+var TILESETS = {
+  "arcgis": {
+    url: 'https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}',
+    attributes: {
+      attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ',
+      minZoom: 2,
+      maxZoom: 10,
+      ext: 'png',
+    },
+  },
+  "usgs": {
+    url: 'https://basemap.nationalmap.gov/arcgis/rest/services/USGSImageryOnly/MapServer/tile/{z}/{y}/{x}',
+    attributes: {
+      maxZoom: 20,
+      attribution: 'Tiles courtesy of the <a href="https://usgs.gov/">U.S. Geological Survey</a>',
+    },
+  },
+  "esri.shaded": {
+    url: 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Shaded_Relief/MapServer/tile/{z}/{y}/{x}',
+    attributes: {
+      attribution: 'Tiles &copy; Esri &mdash; Source: Esri',
+      maxZoom: 13,
+    },
+  },
+  "geoportail": {
+    url: 'https://wxs.ign.fr/{apikey}/geoportail/wmts?REQUEST=GetTile&SERVICE=WMTS&VERSION=1.0.0&STYLE={style}&TILEMATRIXSET=PM&FORMAT={format}&LAYER=ORTHOIMAGERY.ORTHOPHOTOS&TILEMATRIX={z}&TILEROW={y}&TILECOL={x}',
+    attributes: {
+      attribution: '<a target="_blank" href="https://www.geoportail.gouv.fr/">Geoportail France</a>',
+      bounds: [
+        [-75, -180],
+        [81, 180],
+      ],
+      minZoom: 2,
+      maxZoom: 19,
+      apikey: 'choisirgeoportail',
+      format: 'image/jpeg',
+      style: 'normal',
+    },
+  },
+  "cartodb.labeled": {
+    url: 'https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png',
+    attributes: {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+      subdomains: 'abcd',
+      maxZoom: 20,
+    },
+  },
+  "cartodb.unlabeled": {
+    url: 'https://{s}.basemaps.cartocdn.com/dark_nolabels/{z}/{x}/{y}{r}.png',
+    attributes: {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+      subdomains: 'abcd',
+      maxZoom: 20,
+    },
+  },
+  "opentopomap": {
+    url: 'https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png',
+    attributes: {
+      maxZoom: 17,
+      attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+    }
+  }
+}
+var BOUNDARIES = {
+  "toner.boundaries": {
+    url: 'https://stamen-tiles-{s}.a.ssl.fastly.net/toner-lines/{z}/{x}/{y}{r}.{ext}',
+    attributes: {
+      attribution:
+        'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      subdomains: 'abcd',
+      minZoom: 0,
+      maxZoom: 20,
+      ext: 'png',
+    },
+  },
+}
+var LABELS = {
+  "toner.labels": {
+    url: 'https://stamen-tiles-{s}.a.ssl.fastly.net/toner-labels/{z}/{x}/{y}{r}.{ext}',
+    attributes: {
+      attribution:
+        'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      subdomains: 'abcd',
+      minZoom: 0,
+      maxZoom: 20,
+      ext: 'png',
+    },
+  }
+}
+
 var leafletMap = null;
-export const getCurrentLeafletMap = function getCurrentLeafletMap(mapContainer, startLat, startLng, startZoom) {
+export const getCurrentLeafletMap = function getCurrentLeafletMap(mapContainer, startLat, startLng, startZoom, tileSetLayer, boundaryLayer, labelLayer) {
   if(!leafletMap){
     leafletMap = L.map(mapContainer, {
         zoomAnimation: false,
@@ -19,15 +111,13 @@ export const getCurrentLeafletMap = function getCurrentLeafletMap(mapContainer, 
         doubleClickZoom: false,
         keyboard: false,
       }).setView([startLat, startLng], startZoom);
-      L.tileLayer(
-        'https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}',
-        {
-          attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ',
-          minZoom: 2,
-          maxZoom: 10,
-          ext: 'png',
-        }
-      ).addTo(leafletMap);
+      L.tileLayer(TILESETS[tileSetLayer].url, TILESETS[tileSetLayer].attributes).addTo(leafletMap);
+      if(boundaryLayer){
+        L.tileLayer(BOUNDARIES[boundaryLayer].url, BOUNDARIES[boundaryLayer].attributes).addTo(leafletMap);
+      }
+      if(labelLayer){
+        L.tileLayer(LABELS[labelLayer].url, LABELS[labelLayer].attributes).addTo(leafletMap);
+      }
       L.svg({ clickable: true }).addTo(leafletMap); // we have to make the svg layer clickable
   }
   return leafletMap;
@@ -91,10 +181,13 @@ export default class NetworkMap {
     const startLat = options.startLat;
     const startLng = options.startLng;
     const startZoom = options.startZoom;
+    const tileSetLayer = options.tileSetLayer;
+    const boundaryLayer = options.boundaryLayer;
+    const labelLayer = options.labelLayer;
     var div = d3.selectAll('#sidebar-tooltip');
 
     //--- grab copy or instantiate the current leaflet map singleton
-    var map = getCurrentLeafletMap(mapContainer, startLat, startLng, startZoom);
+    var map = getCurrentLeafletMap(mapContainer, startLat, startLng, startZoom, tileSetLayer, boundaryLayer, labelLayer);
 
     //--- Initialize the SVG layer
     const overlay = d3.select(map.getPanes().overlayPane);
@@ -130,6 +223,7 @@ export default class NetworkMap {
       }
     }
     var edit_mode = d3.selectAll('#edge_edit_mode').on('click', toggleEdgeEdit);
+    pubsub.PubSub.subscribe("setEdgeEdit", toggleEdgeEdit);
 
     function toggleNodeEdit() {
       var node_button = d3.selectAll('#node_edit_mode');
@@ -147,6 +241,8 @@ export default class NetworkMap {
       }
     }
     var edit_mode = d3.selectAll('#node_edit_mode').on('click', toggleNodeEdit);
+    pubsub.PubSub.subscribe("setNodeEdit", toggleNodeEdit);
+
     var g1 = null;
     var g2 = null;
     var g3 = null;
