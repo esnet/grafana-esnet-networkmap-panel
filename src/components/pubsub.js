6194d1b0620bb36a72b5590c4d0aa24edb517845
James Kafader <jkafader@es.net>
2022-05-08 14:35:24 -0700

    allow for signaling to happen on other 'this'es

--- src/components/pubsub.js
+++ src/components/pubsub.js
@@ -5,14 +5,17 @@ class PrivateMessageBus {
     }
 }
 
-PrivateMessageBus.prototype.subscribe = function(topic, callback){
+PrivateMessageBus.prototype.subscribe = function(topic, callback, context){
     // use toLocaleString as a (good) approximation of a unique hash so we don't
     // resubscribe to the same topic on code load over and over.
     var hash = callback.toLocaleString();
+    if(!context){
+        context = this;
+    }
     if(!this.topics[topic]){
-        this.topics[topic] = {hash: callback}
+        this.topics[topic] = {hash: {"callback": callback, "context":context }};
     } else {
-        this.topics[topic][hash] = callback;
+        this.topics[topic][hash] = {"callback": callback, "context":context };
     }
 }
 
@@ -23,7 +26,8 @@ PrivateMessageBus.prototype.publish = function(topic, eventData){
     if (!subscriberData) return;
     var subscribers = Object.values(subscriberData);
     for(var i=0; subscribers && i<subscribers.length; i++){
-        subscribers[i](eventData);
+        // use javascript's 'call' property on functions to reassign "this" to "context"
+        subscribers[i]["callback"].call(subscribers[i]["context"], eventData);
     }
 }
 
@@ -33,6 +37,11 @@ PrivateMessageBus.prototype.clearAllCallbacks = function() {
     for(var i=0; i<keys.length; i++){
         this.topics[keys[i]] = {};
     }
+};
+
+PrivateMessageBus.prototype.clearTopicCallbacks = function(topic) {
+    console.log("destroying callbacks on topic '"+topic+"'");
+    if(this.topics[topic]) this.topics[topic] = {};
 };
 
 // returns the last eventData value for a particular topic
