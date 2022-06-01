51f43d1f72f6714a08a7f6f4c6f0e1c6d8a5ba38
James Kafader <jkafader@es.net>
2022-06-01 11:06:56 -0700

    move JSON schema validation here

--- src/components/lib/utils.js
+++ src/components/lib/utils.js
@@ -21,3 +21,113 @@ export function getUrlSearchParams() {
   }
   return params;
 }
+
+const schema = {
+  type: 'object',
+  properties: {
+    edges: {
+      type: 'array',
+      items: {
+        type: 'object',
+        properties: {
+          name: { type: 'string' },
+          latLngs: { type: 'array', items: { type: 'array', items: { type: 'number' } } },
+          meta: {
+            type: 'object',
+            properties: { endpoint_identifiers: { type: 'object' } },
+          },
+        },
+        required: ['name', 'latLngs', 'meta'],
+      },
+    },
+    nodes: {
+      type: 'array',
+      items: {
+        type: 'object',
+        properties: {
+          name: { type: 'string' },
+          latLng: { type: 'array', items: { type: 'number' } },
+        },
+        required: ['name', 'latLng'],
+      },
+    },
+  },
+  required: ['edges', 'nodes'],
+  additionalProperties: true,
+};
+
+// helper function for JSON validation: works on arrays
+const validateArray = function(itemSchema, data){
+  var output = { 'valid': true, errorDetails: {} }
+  for(var i=0; i<data.length; i++){
+    var { valid, errorDetails } = validate(itemSchema.items, data[i]);
+    output['valid'] = valid && output['valid'];
+    output['errorDetails'] = errorDetails;
+    if(!valid){
+      return output;
+    }
+  }
+  return output
+}
+
+// main JSON validation function: heavily recursive
+const validate = function(schema, data){
+  // do type check
+  var output = { "valid": true, "errorDetails": {} }
+  const typeCheckers = {
+    'object': (data)=>{ return typeof(data) === "object"; },
+    'number': (data)=>{ return typeof(data) === "number" },
+    'string': (data)=>{ return typeof(data) === "string" || data instanceof String; },
+    'boolean': (data)=>{ return typeof(data) === 'boolean' },
+    'null': (data)=>{ return data === null },
+    'array': Array.isArray
+  }
+  if(!typeCheckers[schema['type']](data)){
+    var before = output['valid'];
+    output["valid"] = false;
+    output.errorDetails[''] = "type check failed."
+    return output;
+  }
+  // recursive call for each property listed above
+  var props = schema.properties ? Object.keys(schema.properties) : [];
+  for(var i=0; i<props.length; i++){
+      var propName = props[i];
+      if(!data[propName] && schema.required.indexOf(propName)>=0){
+        var before = output['valid'];
+        output['valid'] = false;
+        output['errorDetails'][propName] = "required property '"+propName+"' is not set";
+        return output;
+      }
+  }
+  props.forEach((propName)=>{
+      var valid = true;
+      var errorDetails = "";
+      switch(schema.properties[propName]['type']) {
+        case 'array':
+          var interimOutput = validateArray(schema.properties[propName], data[propName]);
+          valid = interimOutput['valid'];
+          errorDetails = interimOutput['errorDetails'];
+          break;
+        default:
+          var interimOutput = validate(schema.properties[propName], data[propName]);
+          valid = interimOutput['valid'];
+          errorDetails = interimOutput['errorDetails'];
+      }
+      var before = output['valid'];
+      output['valid'] = !!(valid && output['valid']);
+      Object.keys(errorDetails).forEach((childProp)=>{
+        var accessor = childProp == "" ? propName : propName + "." + childProp;
+        output['errorDetails'][accessor] = errorDetails[childProp];
+      })
+  })
+  return output;
+}
+
+export function testJsonSchema(data) {
+  const {valid, errorDetails} = validate(schema, data);
+  if (valid) {
+    return [valid, 'valid'];
+  } else {
+    return [valid, errorDetails];
+  }
+}
