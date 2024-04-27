57f04a2369fdac93901f6235c8bcbdfba27de445
James Kafader <jkafader@es.net>
2024-04-26 18:26:10 -0700

    remove dependency on leaflet globals

--- karma.conf.js
+++ karma.conf.js
@@ -12,8 +12,6 @@ module.exports = function( config ) {
             { pattern: "test/*.spec.js", type: "module" },
             // helper files in tests
             { pattern: "test/*.js", type: "module"},
-            // manually include the file that defines Leaflet as `window['L']` first.
-            { pattern: "src/components/lib/leaflet.global.js", type: "module", included: true, served: true },
             // files tests rely on
             { pattern: "src/**/*.js", type: "module", included: true, served: true },
         ],
