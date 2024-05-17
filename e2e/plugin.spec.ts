aa6aa17a9e7c098c04405b00363d71a88ee995ff
Elton Sanchez <3165000+sanchezelton@users.noreply.github.com>
2024-05-17 15:00:20 -0700

    - Fixes related to merge conflicts to resolve signature of getEditNetworkMapPanelUrl in plugin.spec module

--- e2e/plugin.spec.ts
+++ e2e/plugin.spec.ts
@@ -18,14 +18,12 @@ const getHomepageUrl = async (orgId?: string | number) => {
   }
 }
 
-const getEditNetworkMapPanelUrl = async (targetDashboardUid: string, targetDb: IDashboard | string, panelId: string | number, orgId?: string | number, isEdit = false) => {
+const getEditNetworkMapPanelUrl = async (targetDashboardUid: string, targetDb: IDashboard | string, panelId: string | number, orgId?: string | number) => {
     const { protocolHostPort } = await getHostInfo(credentials);
     const paramObj = {
+      editPanel: panelId,
       'var-node': 'ALBQ',
     };
-    if (isEdit) {
-      paramObj['editPanel'] = panelId;
-    }
     if (!!orgId) {
       paramObj['orgId'] = orgId;
     }
@@ -122,6 +120,7 @@ pluginTest.describe("plugin testing", () => {
   // TODO: add additional e2e tests, may refactor into seperate tests
   pluginTest("load plugin edit page - view options", async ({ page, targets }: { page: Page, targets: ITargets}) => {
     // load plugin edit page
+    const fnName = "plugin.spec['load plugin edit page - view options']";
     const { targetDashboardUid, targetFolder, targetDashboard, targetPanelId, orgId } = targets;
     expect(targetDashboard).toBeDefined();
     const editNetworkMapPanelUrl = `${await getEditNetworkMapPanelUrl(targetDashboardUid, targetDashboard!, targetPanelId, orgId)}`;
@@ -198,76 +197,4 @@ pluginTest.describe("plugin testing", () => {
     // TODO
   });
 
-  pluginTest("confirm node changes upon clicking Grafana's Apply button", async ({ page, targets }: { page: Page, targets: ITargets}) => {
-    const { targetDashboardUid, targetDashboard, targetPanelId, orgId } = targets;
-    const testName = "confirm node changes upon clicking Grafana's Apply button";
-
-    // parameters for test
-    const nodeSelector = ".node .node-B"; // reference to node to operate test upon
-    const dragDelta = { dx: 50, dy: -100 }; // mouse drag parameters
-
-    // go to view mode
-    const viewNetworkMapPanelUrl = getNetworkMapPanelUrl(targetDashboardUid, targetDashboard, targetPanelId, orgId);
-    await page.goto(viewNetworkMapPanelUrl);
-
-    // get pre edit view node reference
-    const preEditViewNodeEl = await page.waitForSelector(nodeSelector);
-    const preEditViewNodeBox = (await preEditViewNodeEl.boundingBox())!;
-    const originalViewCenterX = preEditViewNodeBox.x + preEditViewNodeBox.width / 2;
-    const originalViewCenterY = preEditViewNodeBox.y + preEditViewNodeBox.height / 2;
-
-    // go to edit mode
-    const editNetworkMapPanelUrl = getNetworkMapPanelUrl(targetDashboardUid, targetDashboard, targetPanelId, orgId, true);
-    await page.goto(editNetworkMapPanelUrl);
-
-    // toggle buttons to edit nodes, not edges
-    await page.waitForSelector(nodeSelector);
-    const toggleEditEdgesBtn = await page.getByRole('button', { name: 'Edit Edges: On'});
-    await toggleEditEdgesBtn.click();
-    const toggleEditNodesBtn = await page.getByRole('button', { name: 'Edit Nodes: Off'});
-    await toggleEditNodesBtn.click();
-
-    // get a reference to an edit node
-    const preEditNodeLocator = await page.locator(nodeSelector);
-    const preEditNodeElHandle = await preEditNodeLocator.first();
-    const preEditBox = (await preEditNodeElHandle?.boundingBox())!; // relative to browser viewport
-
-     // the containing bounding box encompasses the node and start of label, so only a quarter of width is needed for center offset of node
-    const preEditBoxCenterX = preEditBox.x - preEditBox.width / 2;
-    const preEditBoxCenterY = preEditBox.y;
-
-    // drag and drop node in edit mode to change its position
-
-    await preEditNodeElHandle.dragTo(preEditNodeElHandle, {
-      force: true,
-      targetPosition: {
-        x: dragDelta.dx,
-        y: dragDelta.dy,
-      }
-    });
-    await page.waitForTimeout(800);
-    const movedNode = await page.locator(nodeSelector);
-    const movedNodeBox = (await movedNode?.boundingBox())!;
-    const postMoveBoxCenterX = movedNodeBox.x - movedNodeBox.width / 2;
-    const postMoveBoxCenterY = movedNodeBox.y;
-    expect(postMoveBoxCenterX).not.toBe(preEditBoxCenterX);
-    expect(postMoveBoxCenterY).not.toBe(preEditBoxCenterY);
-
-    // click apply button directly, without exiting edit mode, should navigate to view mode
-    const applyButton = await page.getByRole("button", { name: /apply/i});
-    await applyButton.click();
-
-    // apply button, defying convention goes directly to view mode after saving changes (and save btn saves, but stays in edit mode)
-    await page.waitForSelector('nav');
-    expect(page).not.toHaveTitle(/edit panel/i);
-
-    // verify that the ctrl point for the B node is not at original position
-    const postEditNodeLocator = await page.locator(nodeSelector);
-    const postEditViewNodeBox = (await postEditNodeLocator.boundingBox())!;
-    const newViewCenterX = postEditViewNodeBox.x + postEditViewNodeBox.width / 2;
-    const newViewCenterY = postEditViewNodeBox.y + postEditViewNodeBox.height / 2;
-
-    expect(newViewCenterX).not.toBe(originalViewCenterX);
-    expect(newViewCenterY).not.toBe(originalViewCenterY);
-  });
 });
