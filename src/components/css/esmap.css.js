78f5fa42a7a688ceb00256d1bfa7a6d961bb5358
James Kafader <jkafader@es.net>
2024-09-04 16:16:37 -0700

    fix :where() selectors. Negotiate with '@grafana/create-plugin update' on packages.

--- src/components/css/esmap.css.js
+++ src/components/css/esmap.css.js
@@ -183,8 +183,8 @@ export const esmapCss = `
 }
 
 /* a 0-specificity class selector. Allows override for background elements by grafana's version of this class */
-#\${instanceId} div:where(.tight-form-func) { background: #FFF; }
-#\${instanceId} a:where(.tight-form-func) { background: #FFF; }
+div:where(.tight-form-func) { background: #FFF; }
+a:where(.tight-form-func) { background: #FFF; }
 
 #\${instanceId} .animated-node { 
   transform: scale(1.5, 1.5);
