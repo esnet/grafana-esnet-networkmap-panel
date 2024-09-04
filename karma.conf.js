77245a0da38d8a20e1d0ac65c1dd372478d7affa
James Kafader <jkafader@es.net>
2024-09-04 12:00:23 -0700

    switch over to 'jasmine' test runner

--- karma.conf.js
+++ karma.conf.js
@@ -3,15 +3,21 @@ const Path = require( "path" );
 module.exports = function( config ) {
     config.set( {
         frameworks: [
-            "mocha",
-            "should",
+            "jasmine",
         ],
-
+        client: {
+            jasmine: {
+                random: false
+            }
+        },
         files: [
             // tests
-            { pattern: "test/*.spec.js", type: "module" },
+            { pattern: "test/module.spec.js", type: "module" },
+            { pattern: "test/esmap.spec.js", type: "module" },
+            { pattern: "test/pubsub.spec.js", type: "module" },
             // helper files in tests
-            { pattern: "test/*.js", type: "module"},
+            { pattern: "test/utils.js", type: "module"},
+            { pattern: "test/constants.js", type: "module"},
             // files tests rely on
             { pattern: "src/**/*.js", type: "module", included: true, served: true },
             { pattern: "src/**/*.mjs", type: "module", included: true, served: true },
@@ -20,10 +26,7 @@ module.exports = function( config ) {
             "src/components/lib/leaflet.js",
             "src/components/old/*"
         ],
-
         browsers: ["ChromeHeadless"],
-
         singleRun: false
-
     } );
 };
