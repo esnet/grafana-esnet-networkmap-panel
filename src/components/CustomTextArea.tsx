314835916bc2dded506615a7a876045a64c99738
Elton Sanchez <3165000+sanchezelton@users.noreply.github.com>
2024-10-03 11:15:04 -0700

    TERR-425 - added sourceMap prop to tsconfig - added doInsert, doDelete fn to support fixing of direct edit issues - added test stub for "invokes the onChange callback without transforming the argument's symbols into HTML entities upon the keyDown event" into CustomTextArea.spec

--- src/components/CustomTextArea.tsx
+++ src/components/CustomTextArea.tsx
@@ -1,6 +1,6 @@
 import React, { useCallback, useEffect, useRef } from 'react';
 
-import { StringFieldConfigSettings } from '@grafana/data';
+import { StandardEditorsRegistryItem, StringFieldConfigSettings } from '@grafana/data';
 import { TextArea } from '@grafana/ui';
 import { monospacedFontSize } from '../options';
 
@@ -14,7 +14,87 @@ function unescape(str) {
     .replace(/&amp;/g, '&')
     .replace(/&lt;/g, '<')
     .replace(/&gt;/g, '>')
-    .replace(/&quot;/g, '"');
+    .replace(/&quot;/g, "'");
+}
+
+const CONTROL_KEYS: (string | RegExp)[] = [
+  'unidentified',
+  /alt.*/i,
+  'control',
+  /fn.*/i,
+  'hyper',
+  'meta',
+  /.*lock/i,
+  /symbol.*/i,
+  'shift',
+  'super',
+  'symbol',
+  'os'
+];
+const NAVIGATION_KEYS: (string | RegExp)[] = [
+  /arrow.*/i,
+  'end',
+  'home',
+  /page.*/i
+];
+
+const doInsert = (currentInput: string, evt: React.KeyboardEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+  const { value } = evt.currentTarget;
+  const selectionStart = evt.currentTarget.selectionStart ?? value.length;
+  const selectionEnd   = evt.currentTarget.selectionEnd ?? value.length;
+
+  let newInChar = evt.key;
+  if (evt.key === 'Enter') {
+    newInChar = '\n';
+  }
+
+  if (selectionStart == selectionEnd) {
+    const insertPos = selectionStart;
+    return `${currentInput.substring(0, insertPos)}${unescape(newInChar)}${currentInput.substring(insertPos + 1)}`;
+  } else {
+    const lowerBoundSelection = selectionStart < selectionEnd ? selectionStart : selectionEnd;
+    const upperBoundSelection = selectionStart > selectionEnd ? selectionStart : selectionEnd;
+    return `${currentInput.substring(0, lowerBoundSelection)}${unescape(newInChar)}${currentInput.substring(upperBoundSelection)}`;
+  }
+};
+
+/**
+ * Modifies a currentInput string based on an evt KeyboardEvent such that if a selection is noted in the event,
+ * the string returned is the currentInput string with the selection within removed.
+ *
+ * If a selection is not present in the event, then a single character is removed relative to the position of
+ * the cursor, with a 'backspace' KeyboardEvent removing the character before the cursor and 'delete' removing
+ * the chracter at the cursor's position.
+ *
+ * @param {string} currentInput
+ * @param {React.KeyboardEvent<HTMLInputElement | HTMLTextAreaElement>} evt
+ * @return The modified string
+ */
+const doDelete = (currentInput: string, evt: React.KeyboardEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+  const lcKey = evt.key.toLowerCase();
+  const { value } = evt.currentTarget;
+  const selectionStart = evt.currentTarget.selectionStart ?? value.length;
+  const selectionEnd   = evt.currentTarget.selectionEnd ?? value.length;
+
+  if (selectionStart == selectionEnd) {
+    if (lcKey == 'backspace') {
+      return `${currentInput.substring(0, Math.max(selectionStart - 1, 0))}${currentInput.substring(selectionEnd)}`;
+    } else if (lcKey == 'delete') {
+      return `${currentInput.substring(0, selectionStart)}${currentInput.substring(Math.min(selectionEnd + 1, value.length - 1))}`;
+    } else {
+      throw new Error(`CustomTextArea.doDelete: Unsupported key event "${lcKey}"}`);
+    }
+  } else {
+    const lowerBoundSelection = selectionStart < selectionEnd ? selectionStart : selectionEnd;
+    const upperBoundSelection = selectionStart > selectionEnd ? selectionStart : selectionEnd;
+    return `${currentInput.substring(0, lowerBoundSelection)}${currentInput.substring(upperBoundSelection)}`;
+  }
+}
+
+interface CustomTextAreaProps {
+  value: string;
+  onChange: (newValue?: string) => void;
+  item: StandardEditorsRegistryItem<any, any>;
 }
 
 /**
@@ -26,21 +106,61 @@ function unescape(str) {
  * Supports the setting 'isMonospaced' to render the text field using a monospaced font, as opposed to a proportional font.
  *
  * @prop {string} value                                           The current value of the standard registry item
- * @prop {(value?: any): void} onChange                           A callback invoked when the value in the text area changes.
+ * @prop {(newValue?: string): void} onChange                     A callback invoked when the value in the text area changes.
  * @prop {StandardEditorsRegistryItem<TValue, TSettings>} item    The standard registry item to edit
  * @returns
  */
-export const CustomTextArea = ({ value, onChange, item }) => {
+export const CustomTextArea = ({ value, onChange, item }: CustomTextAreaProps) => {
     let textareaRef = useRef<HTMLTextAreaElement>(null);
 
   const onValueChange = useCallback(
     (e: React.SyntheticEvent) => {
+      console.log("CustomTextArea.onValueChange invoked!");
       let nextValue = value ?? '';
       if (e.hasOwnProperty('key')) {
         // handling keyboard event
-        const evt = e as React.KeyboardEvent<HTMLInputElement>;
-        if (evt.key === 'Enter' && !item.settings?.useTextarea) {
-          nextValue = unescape(evt.currentTarget.value.trim());
+        console.log("CustomTextArea.onValueChange: has own key prop!");
+        const evt = e as React.KeyboardEvent<HTMLInputElement | HTMLTextAreaElement>;
+        const lcKey = evt.key.toLowerCase();
+        console.log(`CustomTextArea.onValueChange: key = ${evt.key}`);
+        // if a navigation or control key, we do not need to concern ourselves with it
+        if (
+          CONTROL_KEYS.find(ctKey => ctKey instanceof RegExp ? ctKey.test(lcKey) : lcKey == ctKey) ||
+          NAVIGATION_KEYS.find(navKey => navKey instanceof RegExp ? navKey.test(lcKey) : lcKey == navKey)
+        ) {
+          console.log("CustomTextArea.onValueChange: control or navigation key!");
+          // no changes
+          onChange(evt.currentTarget.value);  // no change, but must invoke callback
+          return;
+        }
+        else {
+          console.log("CustomTextArea.onValueChange: NOT a control or navigation key!");
+          e.preventDefault();
+          // if no selection, insert key value at cursor
+          const { value } = evt.currentTarget;
+
+          // handle editing keys
+          if (['Backspace', 'Delete'].includes(evt.key.toLowerCase())) {
+            console.log("CustomTextArea.onValueChange: Invoking doDelete!");
+            nextValue = doDelete(value, evt);
+          } else {
+            console.log("CustomTextArea.onValueChange: Invoking doInsert!");
+            nextValue = doInsert(value, evt);
+          }
+          console.log(`CustomTextArea.onValueChange: nextValue = ${nextValue}`);
+
+          // if (typeof(selectionStart) == 'number' && typeof(selectionEnd) == 'number') {
+          //   // handles both no selection (lower and upper bounds being equal) and selection range (not equal)
+          //   const lowerBoundSelection = selectionStart <= selectionEnd ? selectionStart : selectionEnd;
+          //   const upperBoundSelection = selectionStart <= selectionEnd ? selectionEnd : selectionStart;
+          //   nextValue = `${value.substring(0, lowerBoundSelection)}${unescape(newInChar)}${value.substring(upperBoundSelection)}`;
+          // } else if (typeof(selectionStart) == 'number' && typeof(selectionEnd) != 'number') {
+          //   // may need to be implemented for some browsers
+          //   nextValue = `${value.substring(0, selectionStart)}${unescape(newInChar)}`;
+          // } else if (typeof(selectionStart) != 'number' && typeof(selectionEnd) == 'number') {
+          //   // may need to be implemented for some browsers
+          //   nextValue = `${unescape(newInChar)}${value.substring(selectionEnd)}`;
+          // }
         }
       } else {
         // handling form event
@@ -48,7 +168,9 @@ export const CustomTextArea = ({ value, onChange, item }) => {
         nextValue = unescape(evt.currentTarget.value.trim());
       }
       if (nextValue === value) {
-        return; // no change
+        // no changes
+        // onChange(value);  // no change, but must invoke callback
+        return;
       }
       onChange(nextValue === '' ? undefined : nextValue);
     },
@@ -79,7 +201,7 @@ export const CustomTextArea = ({ value, onChange, item }) => {
       defaultValue={value || ''}
       rows={(item.settings?.useTextarea && item.settings.rows) || 5}
       onBlur={onValueChange}
-      onKeyDown={onValueChange}
+      onChange={onValueChange}
       ref={textareaRef}
     />
   );
--- tests/CustomTextArea.spec.tsx
+++ tests/CustomTextArea.spec.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { fireEvent, render, screen, waitFor } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
+import { userEvent } from '@testing-library/user-event';
 import '@testing-library/dom';
 import { CustomTextArea, CustomTextAreaSettings } from '../src/components/CustomTextArea';
 import { StandardEditorsRegistryItem } from '@grafana/data';
@@ -29,7 +30,7 @@ describe('CustomTextArea', () => {
     expect(screen.getByText('Some default value')).not.toBeUndefined();
   });
 
-  it('passes the data entered wtih', async () => {
+  it("invokes the onChange callback without transforming the argument's symbols into HTML entities upon the change event", async () => {
     const onChangeCb = jest.fn();
     const unEscapedText = "Git's diffs are known for using <<< & >>> as tokens.";
     const component = render(
@@ -50,5 +51,35 @@ describe('CustomTextArea', () => {
 
     expect(onChangeCb).toHaveBeenCalled();
     expect(onChangeCb).toHaveBeenCalledWith(unEscapedText);
+  });
+
+  it.skip("invokes the onChange callback without transforming the argument's symbols into HTML entities upon the keyDown event", async () => {
+    const user = userEvent.setup();
+    let i = 0;
+    const onKeyDownCb = jest.fn((arg) => {
+      console.log(`Jest.fn Invocation #${i++} with arg = ${arg}`);
+    });
+    const unEscapedText = "Git's diffs are known for using <<< & >>> as tokens.";
+    const unEscapedTextLen = unEscapedText.length;
+    const component = render(
+      // @ts-ignore: bad typing from React.FC or React.VFC; potentially resolved with React 18
+      <CustomTextArea
+        value={""}
+        onChange={onKeyDownCb}
+        item={mockItem}
+      />
+    );
+
+    const inputEl: HTMLElement = await component.findByRole('textbox');
+    expect(inputEl.tagName.toLowerCase()).toBe('textarea');
+    const textareaEl: HTMLTextAreaElement = inputEl as HTMLTextAreaElement;
+
+    await user.type(textareaEl, unEscapedText);
+    await fireEvent.blur(textareaEl);
+
+    expect(onKeyDownCb).toHaveBeenCalled();
+    for (let i = 1; i <= unEscapedTextLen; i++) {
+      expect(onKeyDownCb).toHaveBeenNthCalledWith(i, unEscapedText[i - 1]);
+    }
   })
 });
