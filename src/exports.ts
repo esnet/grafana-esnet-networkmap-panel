e08d8fbd28f6afe8eab7573e212e9987d74018ef
James Kafader <jkafader@es.net>
2024-11-06 09:18:36 -0500

    remove this testing-oriented change

--- src/exports.ts
+++ src/exports.ts
@@ -1,9 +1,7 @@
 import { MapCanvas } from './components/MapCanvas.component.js';
 import { PubSub } from './components/lib/pubsub.js';
 import { signals } from './signals.js';
-import { CustomTextArea } from './components/CustomTextArea';
 
 export { MapCanvas };
 export { PubSub };
 export { signals };
-export { CustomTextArea };
