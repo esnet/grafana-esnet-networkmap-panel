0fbc1bc42de4971c09c687fb4ef954394ad94360
James Kafader <jkafader@es.net>
2024-11-15 00:20:59 -0800

    remove spurious dist files

--- dist/components/hoc/withThemeWrapper.d.ts
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-/**
- * High order component HOC for wrapping useTheme2 hook to enable use with React class components.
- * @param {Component} Component
- * @returns {Component} The same component, wrapped with the useTheme2 hook from Grafana.
- */
-export declare function withTheme(Component: any): (props: any) => React.JSX.Element;
-//# sourceMappingURL=withThemeWrapper.d.ts.map
